bafea1fa32788e6339c4ea6ed149ee26704e6c72
Michael <lim_venegas@hotmail.com>
2018-03-02 22:35:51 -0500

    Agrega Auditoria Tabla Company

--- DemoRamas/Entities/Company.cs
+++ DemoRamas/Entities/Company.cs
@@ -1,8 +1,13 @@
-﻿namespace DemoRamas.Entities
+﻿using System;
+
+namespace DemoRamas.Entities
 {
     public class Company
     {
         public string CompanyId { get; set; }
         public string Descripcion { get; set; }
+
+        public string User_Create { get; set; }
+        public DateTime Date_Create { get; set; }        
     }
 }
